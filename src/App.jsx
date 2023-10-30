66d89a8d7b1911acaf9d6fb18574585bb06f7196
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2023-10-31 01:35:43 +0200

    initial commit (create main files for the project)

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import MainSection from "./MainSection";
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <MainSection>
+        <p>1/15</p>
+        <p>Question?</p>
+      </MainSection>
+    </div>
+  );
+}
+
+export default App;
