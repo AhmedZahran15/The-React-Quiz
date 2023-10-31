3d9e4393e05ef3382aa2358f881e49908901689b
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2023-10-31 05:34:22 +0200

    implement next button

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,13 +1,25 @@
-function Options({ question}) {
-    return (
-        <div className="options">
-                {question.options.map((option) => (
-                    <button className="btn btn-option" key={option}>
-                        {option}
-                    </button>
-                ))}
-            </div>
-    )
+function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option${answer === index ? " active" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? " correct"
+                : "wrong"
+              : ""
+          }`}
+          disabled={hasAnswered}
+          key={option}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
 }
 
-export default Options
+export default Options;
