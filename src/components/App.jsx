455e7fc8e2e3546d398012310281459de9642e73
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2023-10-31 03:59:41 +0200

    build question component

---
+++ src/components/App.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import MainSection from "./MainSection";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+const initialState = {
+  questions: [],
+  status: "loading",
+  index: 0,
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, status: "ready", questions: action.payload };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+function App() {
+  const [{ questions, status,index }, dispatch] = useReducer(reducer, initialState);
+  const numberOfQuestions = questions.length;
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <MainSection>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen
+            numberOfQuestions={numberOfQuestions}
+            dispatch={dispatch}
+          />
+        )}
+        {status === "active" && <Question question={questions[index]} />}
+      </MainSection>
+    </div>
+  );
+}
+
+export default App;
